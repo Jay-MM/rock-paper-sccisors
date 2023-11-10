9ae0f4ed5cb41ffc1ff7a70b7416378a0c614a4e
Jesus M <marquez.jay444@gmail.com>
2023-11-09 22:05:24 -0600

    writes pseudo-code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("yoo!")
+// define array to hold choices [rock,paper,scissors]
+//define var for player choice
+// define var for computer choice
+// define var for wins
+// define var for  losses
+// define var for  ties
+
+// prompt  player to choose
+
+// computer chooses randomly 
+
+// compare choices 
+
+
+//  display (alert) ccomparison result ( win!, Tie, Loose  round)
+
+// show stats( number of wins , losses, ties)
+
+// play again 
+  // restartgame
+// else
+  // end game 
