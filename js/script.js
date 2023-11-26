7baf48a32835d0308834061cc74abfccc536c2f5
Jesus M <marquez.jay444@gmail.com>
2023-11-25 21:31:36 -0600

    adds pseudocode to JS  file

--- js/script.js
+++ js/script.js
@@ -1,70 +1,78 @@
+// select elements from DOM
+// declare choice variables
+// declare score variables 
+// create function that starts game
+  // if player choice is R
+// add event listners for each choice to start the game
+
+
 // global variables 
   // array to hold choices [rock,paper,scissors]
-const choices = ["R", "P", "S"];
-  // define var for wins, losses, ties
-let wins = 0;
-let losses = 0;
-let  ties = 0
+// const choices = ["R", "P", "S"];
+//   // define var for wins, losses, ties
+// let wins = 0;
+// let losses = 0;
+// let  ties = 0
 
-// while loop allows the game to continue
-let keepPlaying = true;
+// // while loop allows the game to continue
+// let keepPlaying = true;
 
-while (keepPlaying) {
-// prompt  player to choose
-const playerChoice = prompt('Choose either "R" for Rock, "P" for Paper , or "S" for Scissors');
-console.log('player chooses:' + playerChoice);
+// while (keepPlaying) {
+// // prompt  player to choose
+// const playerChoice = prompt('Choose either "R" for Rock, "P" for Paper , or "S" for Scissors');
+// console.log('player chooses:' + playerChoice);
 
-// computer chooses randomly 
-const random = Math.floor(Math.random()*choices.length);
-const computerChoice = choices[random];
-console.log('computer chooses:' + computerChoice);
+// // computer chooses randomly 
+// const random = Math.floor(Math.random()*choices.length);
+// const computerChoice = choices[random];
+// console.log('computer chooses:' + computerChoice);
 
-// compare choices 
-  // if player choice is equal to computer choice(tie) 
-  if ( playerChoice === computerChoice ){
-    // increase the tie variable by 1 
-    ties++;
-    //  display result 
-    result = "You tied!👔"
-    // also if player choice beats computer choice(win)
-  } else if (
-    // player choice = Rock && computer choice = Scissors
-    ( playerChoice === "R" && computerChoice === "S")||
-    // player choice = Paper && computer choice = Rock
-    ( playerChoice === "P" && computerChoice === "R")||
-    // player choice = Scissors && computer choice = Paper
-    ( playerChoice === "S" && computerChoice === "P")
-    ){
-      // increase the win variable by 1
-      wins++;
-      // display result
-      result = "Congratulations, You Won!🏆🥇"
-      // any other combinations result in the (loss) of the player 
-    } else {
-      // increase the loss variable by 1
-      losses++;
-      // display result
-      result = "Awwww! You lost to a Bot!😭"
-    }
+// // compare choices 
+//   // if player choice is equal to computer choice(tie) 
+//   if ( playerChoice === computerChoice ){
+//     // increase the tie variable by 1 
+//     ties++;
+//     //  display result 
+//     result = "You tied!👔"
+//     // also if player choice beats computer choice(win)
+//   } else if (
+//     // player choice = Rock && computer choice = Scissors
+//     ( playerChoice === "R" && computerChoice === "S")||
+//     // player choice = Paper && computer choice = Rock
+//     ( playerChoice === "P" && computerChoice === "R")||
+//     // player choice = Scissors && computer choice = Paper
+//     ( playerChoice === "S" && computerChoice === "P")
+//     ){
+//       // increase the win variable by 1
+//       wins++;
+//       // display result
+//       result = "Congratulations, You Won!🏆🥇"
+//       // any other combinations result in the (loss) of the player 
+//     } else {
+//       // increase the loss variable by 1
+//       losses++;
+//       // display result
+//       result = "Awwww! You lost to a Bot!😭"
+//     }
     
 
   
 
 
-//  display (alert) ccomparison result ( win!, Tie, Loose  round)
-alert("You" + result)
+// //  display (alert) ccomparison result ( win!, Tie, Loose  round)
+// alert("You" + result)
 
-// show stats( number of wins , losses, ties)
-alert ("Score: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties)
+// // show stats( number of wins , losses, ties)
+// alert ("Score: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties)
 
-// play again
-// restartgame
-// else
-// end game 
-keepPlaying = confirm("Want to play again?")
-if (keepPlaying){
-  continue
-}else{
-  alert("Thanks for playing!")
-}
-}
+// // play again
+// // restartgame
+// // else
+// // end game 
+// keepPlaying = confirm("Want to play again?")
+// if (keepPlaying){
+//   continue
+// }else{
+//   alert("Thanks for playing!")
+// }
+// }
